0c3f86c5ea87d7e3ba653b83fcd85347140c31bd
Yongjie.Zhao <2814551994@qq.com>
2020-04-10 23:11:44 +0800

    algorithm dqn

--- RL-Maze/RL_brain/dqn/dqn_learning.py
+++ RL-Maze/RL_brain/dqn/dqn_learning.py
@@ -1,5 +1,7 @@
 from tools.config import *
+import numpy as np
 from RL_brain.dqn.dq_network import DeepQNetwork
+from RL_brain.dqn.my_model import EvalModel, TargetModel
 
 
 class DQN:
@@ -10,44 +12,50 @@ class DQN:
         """
         开始DQN算法强化学习，智能体移动
         """
-        self.env.buttons_reset(Strings.DQN)                                     # 除按下的按钮外，将其他按钮状态恢复正常
-        self.env.QT = None                                                      # 将Env中的QT对象置空
-        self.env.QT = DeepQNetwork(self.env.n_actions, self.env.n_features,
+        self.env.buttons_reset(Strings.DQN)  # 除按下的按钮外，将其他按钮状态恢复正常
+        self.env.QT = None  # 将Env中的QT对象置空
+        eval_model = EvalModel(num_actions=self.env.n_actions)
+        target_model = TargetModel(num_actions=self.env.n_actions)
+        self.env.QT = DeepQNetwork(self.env.n_actions, self.env.n_features, eval_model, target_model,
+                                   double_q=False,
                                    learning_rate=0.01,
                                    reward_decay=0.9,
-                                   e_greedy=0.9,                # 最大Q值动作决策的最大概率90%
+                                   e_greedy=0.9,
                                    replace_target_iter=200,
-                                   memory_size=2000,
-                                   batch_size=40,
-                                   # e_greedy_increment=0.05,   # 是否按照指定增长率 动态设置增长最大Q动作决策的概率
+                                   memory_size=1000,
+                                   batch_size=80,
+                                   # e_greedy_increment=0.05,       # 是否按照指定增长率 动态设置增长epsilon
                                    # output_graph=True          # 是否生成tensorflow数据流结构文件，用于再浏览器查看
                                    )
         print("----------Reinforcement Learning with DQN-Learning start:----------")
         self.update()
-        self.env.QT.plot_cost()
+        # self.env.QT.plot_cost()
 
     def update(self):
         button = self.env.find_button_by_name(Strings.DQN)
-        step = 0                    # 记录智能体移动步数
+        step = 0  # 记录智能体移动步数
         last_step = 0
         exit_time = 0
         for episode in range(1000):
-            if not button.status == Status.DOWN:                                                    # 检查按钮状态变化（控制算法执行的开关）
+            if not button.status == Status.DOWN:  # 检查按钮状态变化（控制算法执行的开关）
                 print("DQN-Learning has been stopped by being interrupted")
                 return
             while button.status is Status.DOWN:
 
-                self.env.update_map()                                                               # 环境地图界面刷新
+                self.env.update_map()  # 环境地图界面刷新
 
-                if not self.env.QT or not isinstance(self.env.QT, DeepQNetwork):                    # 检查是否因为切换按钮导致Env中的QT对象发生变换
+                if not self.env.QT or not isinstance(self.env.QT, DeepQNetwork):  # 检查是否因为切换按钮导致Env中的QT对象发生变换
                     print('MazeEnv.QT is None after refresh or its type is not DeepQNetwork, DQN-Learning is stopped')
                     return
 
-                action = self.env.QT.choose_action(self.env.reward_table, self.env.agent)           # 通过强化学习算法选择智能体当前状态下的动作
+                action = self.env.QT.choose_action(self.env.reward_table, self.env.agent)  # 通过强化学习算法选择智能体当前状态下的动作
 
-                observation_, reward = self.env.agent_step(action, network=True)                    # 智能体执行动作后，返回新的状态、即时奖励
+                observation_, reward = self.env.agent_step(action)  # 智能体执行动作后，返回新的状态、即时奖励
 
-                self.env.QT.store_transition(self.env.back_agent, action, reward, self.env.agent)   # 强化学习更新Q表
+                # 将Env中的状态值强制转换为float类型
+                state = np.array(self.env.back_agent).astype(float)
+                next_state = np.array(self.env.agent).astype(float)
+                self.env.QT.store_transition(state, action, reward, next_state)  # 强化学习更新Q表
                 step += 1
 
                 # self.env.QT.learn()
@@ -55,20 +63,20 @@ class DQN:
                 if step >= 200 and (step % 5 == 0):
                     self.env.QT.learn()
 
-                if observation_ is 'terminal':                                      # 若智能体撞墙或到达终点，一次学习过程结束
+                if observation_ is 'terminal':  # 若智能体撞墙或到达终点，一次学习过程结束
                     if self.env.agent == self.env.end:
                         terminal = 'to ***EXIT***'
                         exit_time += 1
-                        score = get_score(step-last_step, CellWeight.FINAL)
+                        score = get_score(step - last_step, CellWeight.FINAL)
                     else:
                         terminal = 'to WALL'
-                        score = get_score(step-last_step, CellWeight.WALL)
+                        score = get_score(step - last_step, CellWeight.WALL)
                     print('{0} time episode has been done with using {1} steps {2} at the score {3}'
-                          .format(episode + 1, step-last_step, terminal, score))
+                          .format(episode + 1, step - last_step, terminal, score))
                     last_step = step
                     break
 
-            self.env.agent_restart()                                                # 智能体复位，准备下一次学习过程
+            self.env.agent_restart()  # 智能体复位，准备下一次学习过程
 
         print("DQN-Learning has been normally finished")
 
@@ -80,4 +88,4 @@ def get_score(step, reward):
     :param reward: 智能体学习结束时获得的奖励
     :return: 总分数
     """
-    return -1 * (step-1) + reward
+    return -1 * (step - 1) + reward
