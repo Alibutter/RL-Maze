52b3e8b79a40332be44e627ae9518c421079e784
Yongjie.Zhao <2814551994@qq.com>
2020-04-24 22:45:20 +0800

    add initial params w,b

--- RL-Maze/RL_brain/dqn/my_model.py
+++ RL-Maze/RL_brain/dqn/my_model.py
@@ -1,11 +1,14 @@
 import tensorflow as tf
 from tensorflow.keras import layers
+weights = tf.random_normal_initializer(0., 0.3)
+bias = tf.constant_initializer(0.1)
 
 
 class EvalModel(tf.keras.Model):
     def __init__(self, num_actions):
         super().__init__('eval_network')
-        self.layer1 = layers.Dense(40, activation='relu', name='layer1')
+        self.layer1 = layers.Dense(20, activation='relu', name='layer1',
+                                   use_bias=True, kernel_initializer=weights, bias_initializer=bias)
         self.logits = layers.Dense(num_actions, activation=None, name='eval_output')
 
     def call(self, inputs):
@@ -18,7 +21,8 @@ class EvalModel(tf.keras.Model):
 class TargetModel(tf.keras.Model):
     def __init__(self, num_actions):
         super().__init__('target_network')
-        self.layer1 = layers.Dense(40, trainable=False, activation='relu', name='layer1')
+        self.layer1 = layers.Dense(20, trainable=False, activation='relu', name='layer1',
+                                   use_bias=True, kernel_initializer=weights, bias_initializer=bias)
         self.logits = layers.Dense(num_actions, trainable=False, activation=None, name='target_output')
 
     def call(self, inputs):
