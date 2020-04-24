f411f35e9bbe40523f8a6c6c4b5d4787562a309a
Yongjie.Zhao <2814551994@qq.com>
2020-04-24 22:55:12 +0800

    change units to 30

--- RL-Maze/RL_brain/dqn/my_model.py
+++ RL-Maze/RL_brain/dqn/my_model.py
@@ -7,7 +7,7 @@ bias = tf.constant_initializer(0.1)
 class EvalModel(tf.keras.Model):
     def __init__(self, num_actions):
         super().__init__('eval_network')
-        self.layer1 = layers.Dense(20, activation='relu', name='layer1',
+        self.layer1 = layers.Dense(30, activation='relu', name='layer1',
                                    use_bias=True, kernel_initializer=weights, bias_initializer=bias)
         self.logits = layers.Dense(num_actions, activation=None, name='eval_output')
 
@@ -21,7 +21,7 @@ class EvalModel(tf.keras.Model):
 class TargetModel(tf.keras.Model):
     def __init__(self, num_actions):
         super().__init__('target_network')
-        self.layer1 = layers.Dense(20, trainable=False, activation='relu', name='layer1',
+        self.layer1 = layers.Dense(30, trainable=False, activation='relu', name='layer1',
                                    use_bias=True, kernel_initializer=weights, bias_initializer=bias)
         self.logits = layers.Dense(num_actions, trainable=False, activation=None, name='target_output')
 
